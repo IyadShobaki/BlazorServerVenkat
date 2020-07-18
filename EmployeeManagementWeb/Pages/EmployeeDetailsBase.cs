b58a46629650a0a4124c2c0cbbf1af612ccd6e25
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-17 20:02:08 -0400

    Blazor Event Handling

--- EmployeeManagementWeb/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagementWeb/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagementModels.Models;
 using EmployeeManagementWeb.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,10 @@ namespace EmployeeManagementWeb.Pages
     {
         public Employee Employee { get; set; } = new Employee();
 
+        // Protected because we just want to access it from the view
+        protected string Coordinates { get; set; }
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
         [Parameter]
@@ -22,5 +27,24 @@ namespace EmployeeManagementWeb.Pages
             Id = Id ?? "1";
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
         }
+
+        protected void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
+        }
+
+        //protected void Mouse_Move(MouseEventArgs e)
+        //{
+        //    Coordinates = $"X = { e.ClientX } Y = { e.ClientY }";
+        //}
     }
 }
