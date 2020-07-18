877449279e1242b5fe760a57e4190c28619568b9
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-18 13:40:06 -0400

    Added Razor Class Library
    Moving CustomInputSelect to the razor class library

--- ComponentsLibrary/CustomInputSelect.cs
+++ ComponentsLibrary/CustomInputSelect.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EmployeeManagementWeb.Pages
+namespace ComponentsLibrary
 {
     public class CustomInputSelect<TValue> : InputSelect<TValue>
     {
