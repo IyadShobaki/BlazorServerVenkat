e8770310a2374365d89e102e57a3a2598416758c
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-19 14:02:24 -0400

    ASP NET Core Identity Setup
    Added new scaffolded item to EmployeeManagementWeb project

---
+++ EmployeeManagementWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EmployeeManagementWeb.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EmployeeManagementWeb.Areas.Identity.IdentityHostingStartup))]
+namespace EmployeeManagementWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EmployeeManagementWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EmployeeManagementWebContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<EmployeeManagementWebContext>();
+            });
+        }
+    }
+}
