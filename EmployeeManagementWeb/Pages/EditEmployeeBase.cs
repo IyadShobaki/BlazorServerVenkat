f1f93a684e31ee5ec0376b8ad540a42bf82e836f
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-18 20:34:27 -0400

    Force Reload Blazor Component

--- EmployeeManagementWeb/Pages/EditEmployeeBase.cs
+++ EmployeeManagementWeb/Pages/EditEmployeeBase.cs
@@ -15,6 +15,8 @@ namespace EmployeeManagementWeb.Pages
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
 
+        public string PageHeaderText { get; set; }
+
         private Employee Employee { get; set; } = new Employee();
 
         public EditEmployeeModel EditEmployeeModel { get; set; } = new EditEmployeeModel();
@@ -37,10 +39,12 @@ namespace EmployeeManagementWeb.Pages
             int.TryParse(Id, out int employeeId);
             if (employeeId != 0)
             {
+                PageHeaderText = "Edit Employee";
                 Employee = await EmployeeService.GetEmployee(int.Parse(Id));
             }
             else
             {
+                PageHeaderText = "Create Employee";
                 Employee = new Employee
                 {
                     DepartmentId = 1,
