7715abb2de3e73b5ccc9034460f86951a964be0b
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-19 14:18:01 -0400

    Cookie Authentication - Login Page

--- EmployeeManagementWeb/Startup.cs
+++ EmployeeManagementWeb/Startup.cs
@@ -28,6 +28,8 @@ namespace EmployeeManagementWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("Identity.Application")
+                .AddCookie();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddAutoMapper(typeof(EmployeeProfile));
@@ -59,6 +61,8 @@ namespace EmployeeManagementWeb
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
